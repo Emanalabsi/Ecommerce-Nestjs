8b1dbb523a9795c0cd188fed2754cfb25da345f3
Emanalabsi <emanabsi30@gmail.com>
2023-09-19 12:28:20 +0300

    restore files & add prodcut module

--- src/modules/products/product.controller.ts
+++ src/modules/products/product.controller.ts
@@ -1,4 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Body, Controller, Get, Injectable, Post } from '@nestjs/common';
+import { CreateProductDto } from './dtos/product-create.dto';
+import { ProductService } from './product.service';
 
+@Controller('products')
 @Injectable()
-export class ProductController {}
+export class ProductController {
+  constructor(private productService: ProductService) {}
+
+  @Get()
+  async getProducts() {
+    const products = await this.productService.getProducts();
+    return products;
+  }
+
+  @Post()
+  async addProduct(@Body() createProductDto: CreateProductDto) {
+    const product = await this.productService.addProduct(createProductDto);
+    return product;
+  }
+}
--- src/modules/products/product.service.ts
+++ src/modules/products/product.service.ts
@@ -1,6 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateProductDto } from './dtos/product-create.dto';
+import { Product } from './entities/product.entity';
 
 @Injectable()
 export class ProductService {
-  constructor() {}
+  constructor(
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>,
+  ) {}
+
+  async getProducts(): Promise<Product[]> {
+    const products = await this.productRepository.find();
+    return products;
+  }
+
+  async addProduct(createProductDto: CreateProductDto): Promise<Product> {
+    const product = this.productRepository.create(createProductDto);
+    await this.productRepository.save(product);
+    return product;
+  }
 }
